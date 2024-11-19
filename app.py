7a75b376fe186c47e896260294060cd47b149cbb
Gabriel Freitas <gabrielluiz42@hotmail.com>
2024-11-19 14:12:12 -0300

    finalizando listagem de restaurantes

--- app.py
+++ app.py
@@ -34,20 +34,27 @@ def cadastrar_novo_restaurante():
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    input('aperte uma tecla para voltar ao menu principal! ')
-    main()
     
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando restaurantes')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
         # opcao_escolhida = int(opcao_escolhida)
 
-        if opcao_escolhida == 1:
+        if opcao_escolhida == 1: 
             cadastrar_novo_restaurante()
             print('Cadastrar restaurante')
         elif opcao_escolhida == 2: 
-            print('Listar restaurantes')
+            listar_restaurantes()
         elif opcao_escolhida == 3: 
             print('Ativar restaurante')
         elif opcao_escolhida == 4: 
